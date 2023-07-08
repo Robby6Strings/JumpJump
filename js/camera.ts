563da61df71706ad1661233156a89dbba6757868
Robby6Strings <rausten93@gmail.com>
2023-07-08 18:01:50 +1200

    improve camera smoothing

--- js/camera.ts
+++ js/camera.ts
@@ -4,16 +4,22 @@ import { Player } from "./player.js"
 export class Camera {
   maxDistance: number = constants.screenHeight / 2
   offsetY: number = 0
+  targetY: number = 0
 
   constructor(private player: Player) {}
 
   tick() {
-    const targetY = this.player.pos.y
-    const playerOffset = this.player.pos.y
-    this.offsetY = 0
-    if (targetY < constants.screenHeight / 2) {
-      this.offsetY = playerOffset - constants.screenHeight / 2
+    this.targetY = this.player.pos.y
+    if (this.targetY > constants.screenHeight / 2) {
+      // player is at start
+      this.targetY = constants.screenHeight / 2
     }
+    this.lerpToTarget()
+  }
+  lerpToTarget() {
+    const target = this.targetY - constants.screenHeight / 2
+    const diff = target - this.offsetY
+    this.offsetY += diff * 0.1
   }
   draw(ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = "red"
